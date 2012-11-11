5dfa4bbd7817c125b9be91d0a5a98ae3d49116d5
Samuel Lewis <samuelewis@gmail.com>
2012-11-10 19:54:04 -0500

    Fixed client form anchoring to allow for resizing

--- StrongholdClient/ClientForm.Designer.cs
+++ StrongholdClient/ClientForm.Designer.cs
@@ -42,32 +42,42 @@
             // 
             // treeDirectory
             // 
-            this.treeDirectory.Location = new System.Drawing.Point(12, 12);
+            this.treeDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeDirectory.Location = new System.Drawing.Point(9, 10);
+            this.treeDirectory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.treeDirectory.Name = "treeDirectory";
-            this.treeDirectory.Size = new System.Drawing.Size(479, 302);
+            this.treeDirectory.Size = new System.Drawing.Size(360, 246);
             this.treeDirectory.TabIndex = 0;
             this.treeDirectory.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeDirectory_AfterSelect);
             // 
             // grpDetails
             // 
+            this.grpDetails.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.grpDetails.Controls.Add(this.btnNewFolder);
             this.grpDetails.Controls.Add(this.btnUpload);
             this.grpDetails.Controls.Add(this.lblFileType);
             this.grpDetails.Controls.Add(this.lblFileName);
             this.grpDetails.Controls.Add(this.btnDownload);
             this.grpDetails.Controls.Add(this.btnDelete);
-            this.grpDetails.Location = new System.Drawing.Point(497, 12);
+            this.grpDetails.Location = new System.Drawing.Point(373, 10);
+            this.grpDetails.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grpDetails.Name = "grpDetails";
-            this.grpDetails.Size = new System.Drawing.Size(247, 271);
+            this.grpDetails.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpDetails.Size = new System.Drawing.Size(185, 220);
             this.grpDetails.TabIndex = 1;
             this.grpDetails.TabStop = false;
             this.grpDetails.Text = "Details";
             // 
             // btnNewFolder
             // 
-            this.btnNewFolder.Location = new System.Drawing.Point(125, 209);
+            this.btnNewFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnNewFolder.Location = new System.Drawing.Point(94, 170);
+            this.btnNewFolder.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnNewFolder.Name = "btnNewFolder";
-            this.btnNewFolder.Size = new System.Drawing.Size(110, 25);
+            this.btnNewFolder.Size = new System.Drawing.Size(82, 20);
             this.btnNewFolder.TabIndex = 5;
             this.btnNewFolder.Text = "New Folder...";
             this.btnNewFolder.UseVisualStyleBackColor = true;
@@ -75,9 +85,11 @@
             // 
             // btnUpload
             // 
-            this.btnUpload.Location = new System.Drawing.Point(9, 240);
+            this.btnUpload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUpload.Location = new System.Drawing.Point(7, 195);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUpload.Name = "btnUpload";
-            this.btnUpload.Size = new System.Drawing.Size(110, 25);
+            this.btnUpload.Size = new System.Drawing.Size(82, 20);
             this.btnUpload.TabIndex = 3;
             this.btnUpload.Text = "Upload...";
             this.btnUpload.UseVisualStyleBackColor = true;
@@ -85,24 +97,28 @@
             // 
             // lblFileType
             // 
-            this.lblFileType.Location = new System.Drawing.Point(6, 45);
+            this.lblFileType.Location = new System.Drawing.Point(4, 37);
+            this.lblFileType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFileType.Name = "lblFileType";
-            this.lblFileType.Size = new System.Drawing.Size(229, 23);
+            this.lblFileType.Size = new System.Drawing.Size(172, 19);
             this.lblFileType.TabIndex = 4;
             // 
             // lblFileName
             // 
-            this.lblFileName.Location = new System.Drawing.Point(6, 22);
+            this.lblFileName.Location = new System.Drawing.Point(4, 18);
+            this.lblFileName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFileName.Name = "lblFileName";
-            this.lblFileName.Size = new System.Drawing.Size(229, 23);
+            this.lblFileName.Size = new System.Drawing.Size(172, 19);
             this.lblFileName.TabIndex = 3;
             this.lblFileName.Text = "refreshing...";
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(9, 209);
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDownload.Location = new System.Drawing.Point(7, 170);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDownload.Name = "btnDownload";
-            this.btnDownload.Size = new System.Drawing.Size(110, 25);
+            this.btnDownload.Size = new System.Drawing.Size(82, 20);
             this.btnDownload.TabIndex = 2;
             this.btnDownload.Text = "Download...";
             this.btnDownload.UseVisualStyleBackColor = true;
@@ -110,9 +126,11 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(125, 240);
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnDelete.Location = new System.Drawing.Point(94, 195);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(110, 25);
+            this.btnDelete.Size = new System.Drawing.Size(82, 20);
             this.btnDelete.TabIndex = 1;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -120,9 +138,11 @@
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(506, 289);
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(380, 235);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(110, 25);
+            this.btnRefresh.Size = new System.Drawing.Size(82, 20);
             this.btnRefresh.TabIndex = 2;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
@@ -130,13 +150,13 @@
             // 
             // ClientForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 326);
+            this.ClientSize = new System.Drawing.Size(566, 265);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.grpDetails);
             this.Controls.Add(this.treeDirectory);
-            this.MaximizeBox = false;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ClientForm";
             this.Text = "Stronghold Client";
             this.Load += new System.EventHandler(this.Form1_Load);
--- StrongholdClient/ClientForm.cs
+++ StrongholdClient/ClientForm.cs
@@ -247,7 +247,7 @@ namespace StrongholdClient
         private void UploadFile(string localPath, string remotePath)
         {
             var length = new FileInfo(localPath).Length;
-            var chunk = 1024 * 16;
+            var chunk = 1024 * 512;
             var count = (int)Math.Ceiling((double)length / (double)chunk);
 
             using (var strm = new FileStream(localPath, FileMode.Open))
